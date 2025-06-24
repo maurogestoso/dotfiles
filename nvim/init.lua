5779f7c49cc8769a0fb60156739d22ed21ff05bb
Mauro Gestoso <maurogestoso@gmail.com>
2025-06-24 02:08:37 +0200

    Setup Telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,8 +1,10 @@
---- set indent to 2 spaces
+-- set indent to 2 spaces
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
+vim.g.mapleader = " "
 
 -- Bootstrap lazy.nvim (https://lazy.folke.io/)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -22,7 +24,13 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  -- Catppuccin https://github.com/catppuccin/nvim
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- Telescope https://github.com/nvim-telescope/telescope.nvim
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  }
 }
 local opts = {}
 
@@ -30,3 +38,9 @@ require("lazy").setup(plugins, opts)
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
